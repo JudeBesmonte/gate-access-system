f1f13ca3ea45db619bb0e03810383c5d7a07dfc6
nanashi <111619020+JudeBesmonte@users.noreply.github.com>
2024-08-17 12:50:55 +0800

    Add files via upload

---
+++ GateAccessSystem/GateAccessSystem/frm_driversLicense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GateAccessSystem
+{
+    public partial class frm_driversLicense : Form
+    {
+        public frm_driversLicense()
+        {
+            InitializeComponent();
+        }
+    }
+}
